a50bb663360902d182cbb06ba53a5319f64cc812
Unubold864 <unubat20201007@gmail.com>
2025-02-23 14:48:53 +0800

    removed

--- travel/frontend/lib/pages/tourist_details_page.dart
+++ travel/frontend/lib/pages/tourist_details_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:frontend/widgets/distance.dart';
 import 'package:ionicons_named/ionicons_named.dart';
 
 class TouristDetailsPage extends StatelessWidget {
@@ -65,95 +64,6 @@ class TouristDetailsPage extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 20),
-
-            // Title and Rating Section
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Sea of Peace",
-                      style: GoogleFonts.poppins(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      "Portic Team • 8km",
-                      style: GoogleFonts.poppins(
-                        fontSize: 14,
-                        color: Colors.grey.shade600,
-                      ),
-                    ),
-                  ],
-                ),
-                Spacer(),
-                Column(
-                  children: [
-                    Text(
-                      "4.6",
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87,
-                      ),
-                    ),
-                    Icon(ionicons['star_outline'], color: Colors.amber, size: 20),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-
-            // Timer Section
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "01d:32h:56m",
-                      style: GoogleFonts.poppins(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      "Started in",
-                      style: GoogleFonts.poppins(
-                        fontSize: 14,
-                        color: Colors.grey.shade600,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-
-            // Map Section
-            Container(
-              height: 120,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.grey.shade100,
-                image: DecorationImage(
-                  image: AssetImage('assets/map.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-
-            // Distance Widget
-            Distance(),
             SizedBox(height: 20),
 
             // Start Tour Button
