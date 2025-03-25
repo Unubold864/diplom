5f17d63bbfd68610458cf0447b495b413c87722d
Unubold864 <unubat20201007@gmail.com>
2025-03-25 16:29:59 +0800

    aa

--- travel/frontend/lib/pages/home_page.dart
+++ travel/frontend/lib/pages/home_page.dart
@@ -158,7 +158,7 @@ class HomeContent extends StatelessWidget {
             const SizedBox(height: 30),
             _buildSectionHeader(
               context,
-              title: "Top Destinations",
+              title: "Recommended Places",
               onViewAll: () {},
             ),
             const SizedBox(height: 20),
